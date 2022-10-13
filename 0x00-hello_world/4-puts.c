844c80d56781f7e57e4c360dfaf528a640ed062d
betsej <bejakpovi@gmail.com>
2022-10-13 10:33:58 -0700

    my betty file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 /**
  *main- Serves as the entry point for the program
  *
@@ -8,8 +9,3 @@ int main(void)
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-
-
-ls
-clear
-ls
