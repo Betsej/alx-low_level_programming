2438979c97191e4cf0555129683a424624196749
betsej <bejakpovi@gmail.com>
2022-10-20 06:14:43 -0700

    task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,18 +7,18 @@
 
 int main(void)
 {
-int main(void)
-
 int i;
+
+printf("Infinite loop incoming : (\n");
 
 i = 0;
 
  /*
-
- * {
- * putchar(i);
- * }
- */
-printf("infinite loop avoided! \\o/\n");
+  * while (i < 10)
+  * {
+  * putchar(i);
+  * }
+  */
+printf("Infinite loop avoided! \\o/\n");
 return (0);
 }
