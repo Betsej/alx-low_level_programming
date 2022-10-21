125aac15b91740e7e23f4fa827efb890fdde738c
betsej <bejakpovi@gmail.com>
2022-10-21 13:03:01 +0100

    nested loops

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -7,18 +7,18 @@
 
  int main(void)
  {
-    long int n, fp;
+long int n, fp;
 
-    n = 612852475143;
-    for (fp = 2; fp <= n; fp++)
-    {
-        if (n % fp == 0)
-        {
-            n /= fp;
-            fp--;
-        }
+n = 612852475143;
+for (fp = 2; fp <= n; fp++)
+{
+if (n % fp == 0)
+{
+n /= fp;
+fp--;
+}
 
-    }
-    printf("%ld\n", fp);
-    return (0);
- }
+}
+printf("%ld\n", fp);
+return (0);
+}
