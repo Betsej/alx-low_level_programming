d7d2acf506c3f4e7340171e8264c45100aba4211
betsej <bejakpovi@gmail.com>
2022-12-05 22:58:48 +0100

    files

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -142,15 +142,11 @@ void print_data(char *ptr)
  */
 void print_magic(char *ptr)
 {
-	int bytes;
-
-	printf("  Magic:  ");
-
-	for (bytes = 0; bytes < 16; bytes++)
-		printf(" %02x", ptr[bytes]);
-
-	printf("\n");
-
+int bytes;
+printf("  Magic:  ");
+for (bytes = 0; bytes < 16; bytes++)
+printf(" %02x", ptr[bytes]);
+printf("\n");
 }
 
 /**
@@ -160,25 +156,21 @@ void print_magic(char *ptr)
  */
 void check_sys(char *ptr)
 {
-	char sys = ptr[4] + '0';
+char sys = ptr[4] + '0';
+if (sys == '0')
+exit(98);
+printf("ELF Header:\n");
+print_magic(ptr);
+if (sys == '1')
+printf("  Class:                             ELF32\n");
+if (sys == '2')
+printf("  Class:                             ELF64\n");
 
-	if (sys == '0')
-		exit(98);
-
-	printf("ELF Header:\n");
-	print_magic(ptr);
-
-	if (sys == '1')
-		printf("  Class:                             ELF32\n");
-
-	if (sys == '2')
-		printf("  Class:                             ELF64\n");
-
-	print_data(ptr);
-	print_version(ptr);
-	print_osabi(ptr);
-	print_type(ptr);
-	print_addr(ptr);
+print_data(ptr);
+print_version(ptr);
+print_osabi(ptr);
+print_type(ptr);
+print_addr(ptr);
 }
 
 /**
@@ -215,13 +207,11 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: elf_header elf_filename\n");
 		exit(98);
 	}
-
 	fd = open(argv[1], O_RDONLY);
-
 	if (fd < 0)
 	{
-		dprintf(STDERR_FILENO, "Err: file can not be open\n");
-		exit(98);
+	dprintf(STDERR_FILENO, "Err: file can not be open\n");
+	exit(98);
 	}
 
 	lseek(fd, 0, SEEK_SET);
